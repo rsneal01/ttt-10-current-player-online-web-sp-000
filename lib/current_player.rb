c03e2d97b9a69df6fd1e31da14e47da1d3848724
Bobby Neal <rsneal01@gmail.com>
2020-04-11 17:33:42 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    if turn = "X" 
+    if turn == "X" 
     counter += 1
-    elsif turn = "O"
+    elsif turn == "O"
     counter += 1
     
 end
